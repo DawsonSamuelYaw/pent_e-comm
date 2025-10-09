7ebe17a6947c9410171022c6574b94632bbe8166
DawsonSamuelYaw <dawsonsam05595@gmail.com>
2025-10-09 20:26:43 +0000

    new changes

--- pent-shop/vite.config.js
+++ pent-shop/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 important for Netlify or any static hosting
   server: {
     port: 5173,
     proxy: {
